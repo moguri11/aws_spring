ecb071405571823009f35bf5ec9249b665eb55d6
moguri <mjy312726@naver.com>
2025-09-19 10:39:51 +0900

    first commit

---
+++ src/main/java/com/example/aws_spring/AwsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.aws_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsSpringApplication.class, args);
+	}
+
+}
